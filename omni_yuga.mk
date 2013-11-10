cbf370ac06206a64b35745ad3b449ac562c6e33b
iKillCypher <xzain69@gmail.com>
2013-11-10 11:26:16 +0800

    Inherit GSM configuration
    Change-Id: Ia796a9d3c41557d0dfa7702e9a1645e777cc8f61

--- omni_yuga.mk
+++ omni_yuga.mk
@@ -1,5 +1,8 @@
+# Inherit GSM configuration
+$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit Omni Product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
-
+# Inherit Yuga Product configuration
 $(call inherit-product, device/sony/yuga/full_yuga.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.2.2/10.3.A.0.423/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.2.2 10.3.A.0.423 WP5_rg test-keys"
