e52ec432a9eaedddd5c280f92929c846ff5ebe1c
Pulser <pulser@xda-developers.com>
2014-01-18 11:55:42 +0100

    yuga: Add OmniTorch to build
    Change-Id: Ib53efc79e47ec197e15ab30512abf37167d0ef1b

--- full_yuga.mk
+++ full_yuga.mk
@@ -27,7 +27,7 @@ TARGET_SCREEN_WIDTH := 1080
  
 # Torch
 PRODUCT_PACKAGES := \
-    Torch
+    OmniTorch
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
