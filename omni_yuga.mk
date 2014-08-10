034ab4384c9ca52b7edd77c98b167c17c90b88f8
olivieer <yves.olli@gmail.com>
2014-08-10 18:42:29 -0400

    bootrec-device: add support for vibration
    And fix the previous commit (sorry).
    
    Change-Id: Id4f57916cd0bf154c98843a8fa641a04e9452dcf

--- omni_yuga.mk
+++ omni_yuga.mk
@@ -5,8 +5,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit Yuga Product configuration
 $(call inherit-product, device/sony/yuga/full_yuga.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.4.
-2/10.5.A.0.230/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="yuga-userdebug 4.4.2 10.5.A.0.230 WP5_rg test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.4.2/10.5.A.0.230/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="yuga-userdebug 4.4.2 10.5.A.0.230 WP5_rg test-keys"
 
 PRODUCT_NAME := omni_yuga
 PRODUCT_DEVICE := yuga
