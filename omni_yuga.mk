0bf6661f0ced98013fd0e6f56af2332b93f41244
olivieer <yves.olli@gmail.com>
2014-09-13 13:41:09 -0400

    yuga: Update build fingerprint (10.5.1.A.0.283)
    Change-Id: I495ed85af50d2b7bf7cb13418503ce9983012c4f

--- omni_yuga.mk
+++ omni_yuga.mk
@@ -1,11 +1,13 @@
-# Inherit GSM configuration
+# Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
-# Inherit Omni Product configuration
+
+# Inherit from our Omni product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
-# Inherit Yuga Product configuration
+
+# Inherit device configuration
 $(call inherit-product, device/sony/yuga/full_yuga.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.4.2/10.5.A.0.230/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="yuga-userdebug 4.4.2 10.5.A.0.230 WP5_rg test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.4.4 10.5.1.A.0.283 5P53rQ release-keys"
 
 PRODUCT_NAME := omni_yuga
 PRODUCT_DEVICE := yuga
