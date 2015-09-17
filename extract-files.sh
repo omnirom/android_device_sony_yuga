72c3a89a12b27af33aa281b546a38cd7b0de3049
Humberto Borba <humberos@gmail.com>
2015-09-17 20:12:44 -0300

    yuga: Fix extracting files script
    This script was broken since fusion3 repo hasn't dependencies of
    common repos anymore.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I53ee25815e4d07eb3b06f30a10ce89a8ac1308b0

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,63 @@ export VENDOR=$(basename $(dirname $FP))
 export DEVICE=$(basename $FP)
 export BOARDCONFIGVENDOR=true
 
-../common/extract-files.sh $@
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 
-../common/setup-makefiles.sh
+while getopts ":nhsd:" options
+do
+  case $options in
+    n ) NC=1 ;;
+    d ) LDIR=$OPTARG ;;
+    s ) SETUP=1 ;;
+    h ) echo "Usage: `basename $0` [OPTIONS] "
+        echo "  -n  No cleanup"
+        echo "  -d  Fetch blob from filesystem"
+        echo "  -s  Setup only, no extraction"
+        echo "  -h  Show this help"
+        exit ;;
+    * ) ;;
+  esac
+done
+
+if [ "x$NC" != "x1" ]; then
+  rm -rf $BASE/*
+fi
+
+for FILE in `grep -v ^# ../$DEVICE/proprietary-files.txt | grep -v ^$ | sort`
+do
+  # Split the file from the destination (format is "file[:destination]")
+  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+  FILE=${PARSING_ARRAY[0]}
+  DEST=${PARSING_ARRAY[1]}
+  if [[ "$FILE" =~ ^-.* ]]; then
+    FILE=`echo $FILE | sed s/^-//`
+  fi
+  if [ -z "$DEST" ]; then
+    DEST=$FILE
+  fi
+  DIR=`dirname $DEST`
+  if [ ! -d $BASE/$DIR ]; then
+    mkdir -p $BASE/$DIR
+  fi
+
+  if [ "x$SETUP" != "x1" ]; then
+    if [ -z $LDIR ]; then
+      adb pull /system/$FILE $BASE/$DEST
+    else
+      cp $LDIR/system/$FILE $BASE/$DEST
+    fi
+    # if file dot not exist try destination
+    if [ "$?" != "0" ]; then
+      if [ -z $LDIR ]; then
+        adb pull /system/$DEST $BASE/$DEST
+      else
+        cp $LDIR/system/$DEST $BASE/$DEST
+      fi
+    fi
+  fi
+done
+
+../fusion3-common/setup-makefiles.sh
+../fusion3-common/extract-files.sh
+
+exit 0
